6810dcaad510e84659ace7944ee833a9feb5dbfa
Grant Meek <grant.meek@mismarine.com>
2017-02-13 10:48:59 +0000

    updted build files

--- build/lib/js/application.js
+++ build/lib/js/application.js
@@ -4,8 +4,6 @@ $(function()
 		'data/json.php', 
 		function(data)
 		{
-			var items = [];
-			
 			//this is a really dirty way to add this - do it properly
 			var $container = $("#player-list");
 			
@@ -27,16 +25,22 @@ $(function()
 					
 					var month = val.number;
 					
-					sb += "<h3><a href='#' class='item-month'>" + val.month + "</a></h3>";
+					sb += "<h3><a href='#' class='item-month' title='" + val.month + ' ' + year + "'>" + val.month + "</a></h3>";
 					
 					sb += "<ul>";
 					
 					$.each(val.songs, function(key, val)
 					{
-						sb += "<li>";
+						var img_path = (val.img == false) ? "http://placekitten.com/g/200/200" : val.img;
 						
-						sb += "<a href='#' data-path='" + val.path + "' class='item-song'>" + val.name + "</a>";
-						sb += "<span>" + val.artist + "</span>";
+						sb += "<li>";
+						sb += "<a href='#' data-path='" + val.path + "' class='item-song'>";
+						sb += "<img src='" + img_path + "'/>";
+						sb += "<span>";
+						sb += "<big>" + val.name + "</big>";
+						sb += "<small>" + val.artist + "</small>";
+						sb += "</span>";
+						sb += "</a>";
 						sb += "</li>";
 					});
 					
@@ -60,18 +64,23 @@ $(function()
 function init_player()
 {
 	// Local copy of jQuery selectors, for performance.
-	var	my_jPlayer = $("#jquery_jplayer"),
-		my_trackName = $("#jp_container .track-name"),
-		my_playState = $("#jp_container .play-state"),
-		my_extraPlayInfo = $("#jp_container .extra-play-info");
+	var	my_jPlayer = $("#jquery_jplayer");
+	var	my_trackName = $("#jp_container .track-name");
+	var	my_playState = $("#jp_container .play-state");
+	var	my_extraPlayInfo = $("#jp_container .extra-play-info");
+	
+	var $date_info = $("#date-info");
+	
+	var $btn_next = $("#btn-next");
+	var $btn_prev = $("#btn-prev");
 	
 	var current_track;
 
 	// Some options
-	var	opt_play_first = false, // If true, will attempt to auto-play the default track on page loads. No effect on mobile devices, like iOS.
-		opt_auto_play = true, // If true, when a track is selected, it will auto-play.
-		opt_text_playing = "Now playing", // Text when playing
-		opt_text_selected = "Track selected"; // Text when not playing
+	var	opt_play_first = false;
+	var	opt_auto_play = true; 
+	var	opt_text_playing = "Now playing";
+	var	opt_text_selected = "Track selected"; 
 
 	// A flag to capture the first track
 	var first_track = true;
@@ -91,7 +100,8 @@ function init_player()
 		{
 			//$("#jp_container .track-default").click();
 		},
-		timeupdate: function(event) {
+		timeupdate: function(event)
+		{
 			my_extraPlayInfo.text(parseInt(event.jPlayer.status.currentPercentAbsolute, 10) + "%");
 		},
 		play: function(event)
@@ -104,19 +114,12 @@ function init_player()
 		},
 		ended: function(event)
 		{
-			my_playState.text(opt_text_selected);
-			
-			current_track = $("a", current_element.parent().next());
-			
-			current_track.click();
-			
+			$btn_next.click();
 		},
 		cssSelectorAncestor: "#jp_container",
 		supplied: "mp3",
 		swfPath:"lib/swf/Jplayer.swf"
 	});
-	
-	var $date_info = $("#date-info");
 	
 	// Create click handlers for the different tracks
 	$("#player-list a.item-song").click(function(e)
@@ -124,6 +127,10 @@ function init_player()
 		current_track = $(this);
 		
 		my_trackName.text(current_track.text());
+		
+		$("#player-list .current").removeClass("current");
+		
+		current_track.addClass("current");
 		
 		my_jPlayer.jPlayer(
 			"setMedia",
@@ -151,7 +158,7 @@ function init_player()
 		return false;
 	});
 	
-	$("#btn-next").click(function(e)
+	$btn_next.click(function(e)
 	{
 		current_track = $("a", current_track.parent().next());
 		
@@ -160,7 +167,7 @@ function init_player()
 		return false;
 	});
 	
-	$("#btn-prev").click(function(e)
+	$btn_prev.click(function(e)
 	{
 		current_track = $("a", current_track.parent().prev());
 		
@@ -169,24 +176,16 @@ function init_player()
 		return false;
 	});
 
-	//$("#jplayer_inspector").jPlayerInspector({jPlayer:$("#jquery_jplayer")});
-	
-	init_tree();
-}
-
-function init_tree()
-{
 	$("a.item-year").click(function(e)
 	{
-		$(this).parent().next().toggle();
+		$(this).parent().next().slideToggle();
 		
 		return false;
 	});
 	
 	$("a.item-month").click(function(e)
 	{
-		$(this).parent().next().toggle();
-		
+		$(this).parent().next().slideToggle();
 		
 		return false;
 	});
